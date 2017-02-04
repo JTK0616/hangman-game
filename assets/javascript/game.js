b2d87a34f8d11f3ce78bce9ceaa449c546f53e5c
Jeff Knowles <JeffKnowles@jeffs-mbp.attlocal.net>
2017-02-03 22:46:12 -0500

    HTML Layout complete and JS begun

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-console.log("This works!");
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        document.getElementById(randomWord)="random_word";
+    }
+}
+
+var wordbank=["Elementary","Playground","Crayons","Glitter", "Backpack", "Recess", "Teacher", "Desk", "Encyclopedia", "Library", "Homeroom"];
+
+var randomWord = wordbank[Math.floor(Math.random() * wordbank.length)];
+
+// var letterguessed = event.key;
+
+

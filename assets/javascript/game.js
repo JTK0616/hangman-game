e3685e103209f81dfff365cf7966fc5508008e23
Jeff Knowles <JeffKnowles@jeffs-mbp.attlocal.net>
2017-02-05 23:22:10 -0500

    Loop generates dashed word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,68 @@
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-        document.getElementById(randomWord)="random_word";
-    }
-}
 
-var wordbank=["Elementary","Playground","Crayons","Glitter", "Backpack", "Recess", "Teacher", "Desk", "Encyclopedia", "Library", "Homeroom"];
+// Define the wordbank to be used in the hangman game
+
+var wordbank=["Elementary","Playground","Crayons","Glitter", "Backpack", "Recess", "Teacher", "Desk", "Encyclopedia", "Library", "Homeroom", "Lunchbox","Classmates","Crosswalk"];
+
+// Randomly choose a word from the wordbank to be guessed
 
 var randomWord = wordbank[Math.floor(Math.random() * wordbank.length)];
 
-// var letterguessed = event.key;
+
+// Start the game and display the word when the spacebr is pressed
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+    	console.log(randomWord)
+    }
+}
+
+var dashes = "";
+
+for (i = 0; i < randomWord.length; i++) {
+  if (randomWord.charAt(i) == " ") {
+    dashes += " ";
+  } else {
+    dashes += "-";
+  }
+}
+
+document.getElementById("dashed_word").innerHTML = (dashes);
 
 
+// Convert word chosen to blanks
+
+console.log(dashes);
+
+document.getElementById("dashed_word").innerHTML = (dashes);
+
+
+// function blanksFromAnswer(randomWord) {
+//   var result = "";
+//   for (var i = 0; i < randomWord.length; i++) {
+//     result += "_";
+//   }
+//   console.log(blanksFromAnswer);
+// }
+
+// function hiddenWord() {
+// 	for (i=0; i<randomWord.length; i++); {
+// 		console.log("_"*[i]);
+// 	}
+// }
+
+// hiddenWord(randomWord);
+
+// hiddenWord(randomWord) {
+// 	console.log();
+// }
+
+// Define Variables
+
+  var guessesLeft = 10;
+  var losses = 0;
+  var wins = 0;
+  var previousGuesses = [];
+
+  // document.getElementById("random_word").innerHTML = (randomWord);
+
+  
